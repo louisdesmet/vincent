1d479f7da76487535b3258844dea8ac00d3a49b9
louis de smet <louisdesmet2@hotmail.com>
2017-12-30 02:02:02 +0100

    next commit

---
+++ resources/views/navigation/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            Create page
+            <a href="{{route('page.create')}}" class="btn btn-primary float-right"><i class="fa fa-fw fa-plus"></i>&nbsp;Add page</a>
+        </div>
+        <div class="card-body">
+            {!! Form::open(array('route' => 'page.store')) !!}
+                {{ Form::bootstrapText('name') }}         
+                <button type="submit" class="btn btn-primary"><i class="fa fa-fw fa-plus"></i>&nbsp;Add</button>
+            {!! Form::close() !!}
+        </div>
+    </div>
+@endsection
---
+++ resources/views/navigation/edit.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            Create page
+            <a href="{{route('page.create')}}" class="btn btn-primary float-right"><i class="fa fa-fw fa-plus"></i>&nbsp;Add page</a>
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+@endsection
